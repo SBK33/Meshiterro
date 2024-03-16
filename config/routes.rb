fcbefb41204ff34b2e8c5f799415b1dcd2ce063e
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-03-16 10:25:28 +0000

    [finish]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'post_images/new'
+  get 'post_images/show'
+  get 'post_images/index'
   devise_for :users
   root to: "homes#top"
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
 
   get 'homes/about' => 'homes#about', as: 'about'
 
