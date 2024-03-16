5154b42c5ac8052bd40d69f43b0725b417829425
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-03-16 07:01:11 +0000

    [finish]4-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+
+#↓deviseのメソッド
   before_action :cofigure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  #(resource)にはUser情報が格納される
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+#↑deviseのメソッド
 
   protected
 
