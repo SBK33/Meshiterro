b998a1520293e003885e6b5de1cc53f2ed876086
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-03-16 06:35:49 +0000

    [finish]4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :cofigure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def cofigure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+
 end
