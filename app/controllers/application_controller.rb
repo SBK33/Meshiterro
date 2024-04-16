f538dd72e19fbb9022eeeb94e5737fc6876f38ef
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-04-16 16:22:22 +0000

    [Finish] 4ex-4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,23 @@
 class Public::ApplicationController < ActionController::Base
 
-#↓deviseのメソッド
-  before_action :authenticate_user!, except: [:top], unless: :admin_controller?
-  before_action :cofigure_permitted_parameters, if: :devise_controller?
-
-  def after_sign_in_path_for(resource)
-    post_images_path
-  end
-  #(resource)にはUser情報が格納される
-  def after_sign_out_path_for(resource)
-    about_path
-  end
-#↑deviseのメソッド
+  before_action :configure_authentication
+ 
   private
-
+ 
+  def configure_authentication
+    if admin_controller?
+      authenticate_admin!
+    else
+      authenticate_user! unless action_is_public?
+    end
+  end
+ 
   def admin_controller?
     self.class.module_parent_name == 'Admin'
   end
-
-  protected
-
-  def cofigure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+ 
+  def action_is_public?
+    controller_name == 'homes' && action_name == 'top'
   end
-
-
+  
 end
